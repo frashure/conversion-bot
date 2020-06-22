cc3cab62901fe2d6eda127197826aab213e1a4ef
Chris <csfrashure@gmail.com>
2020-06-22 19:19:35 -0400

    log event and event type name

--- controllers/troubleshoot.js
+++ controllers/troubleshoot.js
@@ -4,16 +4,17 @@ const controller = {
 
     troubleshoot: (req, res, next) => {
 
-        console.log(req.body);
+        console.log(req.body.webhookEvent);
+        console.log(req.body.issue_event_type_name)
 
 
-        var events = req.body.issue.events;
+        // var events = req.body.issue.events;
 
-        for (i = 0; i < events.length; i++) {
+        // for (i = 0; i < events.length; i++) {
 
-            console.log(events[i]);
+        //     console.log(events[i]);
 
-        }
+        // }
 
         res.status(200).send();
 
