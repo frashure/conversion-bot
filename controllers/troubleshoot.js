151d6b8c22462451bbda2742fa250fef4af20c0e
Chris <csfrashure@gmail.com>
2020-06-22 19:15:00 -0400

    log entire request body

--- controllers/troubleshoot.js
+++ controllers/troubleshoot.js
@@ -4,6 +4,9 @@ const controller = {
 
     troubleshoot: (req, res, next) => {
 
+        console.log(req.body);
+
+
         var events = req.body.events;
 
         for (i = 0; i < events.length; i++) {
