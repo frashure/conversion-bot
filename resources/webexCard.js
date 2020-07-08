5dd559cffadacbd959a420fb4fd77916c897733f
Chris Frashure <csfrashure@gmail.com>
2020-07-08 14:44:46 -0400

    pull payload object out into own file

---
+++ resources/webexCard.js
@@ -0,0 +1,132 @@
+var payload = {
+    "roomId": process.env.roomId,
+    "text": "notification text",
+    "attachments": [
+        {
+        "contentType": "application/vnd.microsoft.card.adaptive",
+        "content": {
+            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+            "type": "AdaptiveCard",
+            "version": "1.0",
+            "body": [
+            {
+                "type": "ColumnSet",
+                "columns": [
+                    {
+                        "type": "Column",
+                        "items": [
+                            {
+                                "type": "Image",
+                                "style": "Person",
+                                "url": 'http://jira.frashure.io/images/bug.png',
+                                "size": "Medium",
+                                "height": "50px"
+                            }
+                        ],
+                        "width": "auto"
+                    },
+                    {
+                        "type": "Column",
+                        "items": [
+                            {
+                                "type": "TextBlock",
+                                "text": "Data Conversion Notification System",
+                                "weight": "Lighter",
+                                "color": "Accent"
+                            },
+                            {
+                                "type": "TextBlock",
+                                "weight": "Bolder",
+                                "text": "Issue Selected for Development",
+                                "horizontalAlignment": "Left",
+                                "wrap": true,
+                                "color": "Light",
+                                "size": "Large",
+                                "spacing": "Small"
+                            }
+                        ],
+                        "width": "stretch"
+                    }
+                ]
+            },
+            {
+                "type": "ColumnSet",
+                "columns": [
+                    {
+                        "type": "Column",
+                        "width": 35,
+                        "items": [
+                            {
+                                "type": "TextBlock",
+                                "text": "Issue Key:",
+                                "color": "Light"
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "Value Stream:",
+                                "weight": "Lighter",
+                                "color": "Light",
+                                "spacing": "Small"
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "Issue Type:",
+                                "weight": "Lighter",
+                                "color": "Light",
+                                "spacing": "Small"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "Column",
+                        "width": 65,
+                        "items": [
+                            {
+                                "type": "TextBlock",
+                                "text": "Issue key goes here",
+                                "color": "Light"
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "None",
+                                "color": "Light",
+                                "weight": "Lighter",
+                                "spacing": "Small"
+                            },
+                            {
+                                "type": "TextBlock",
+                                "text": "Bug",
+                                "weight": "Lighter",
+                                "color": "Light",
+                                "spacing": "Small"
+                            }
+                        ]
+                    }
+                ],
+                "spacing": "Padding",
+                "horizontalAlignment": "Center"
+            },
+            {
+                "type": "TextBlock",
+                "text": "Issue summary goes here.",
+                "wrap": true
+            },
+            {
+                "type": "ActionSet",
+                "horizontalAlignment": "Left",
+                "spacing": "None",
+                "actions": [
+                    {
+                        "type": "Action.OpenUrl",
+                        "title": "View in JIRA",
+                        "url": "http://google.com"
+                    }
+                ]
+            }
+        ]
+        }
+        }
+    ]
+    };
+
+module.exports = payload;
